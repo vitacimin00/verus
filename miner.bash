44e4446954006cb5b70d9ddaf939fa5e043640a6
vitacimin00 <39408873+vitacimin00@users.noreply.github.com>
2025-02-27 15:02:04 +0700

    Update miner.bash

--- miner.bash
+++ miner.bash
@@ -8,7 +8,7 @@ tar -xvf hellminer_linux64.tar.gz
 # Auto-restart Hellminer jika berhenti
 while true; do
     echo "Starting Hellminer..."
-    ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RCizkBnjcFoNFqPTuYkukUWTKaaGS9eac2.vitacimin00 -p x --cpu 1
+    ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RCizkBnjcFoNFqPTuYkukUWTKaaGS9eac2.vitacimin00 -p x --cpu 2
     echo "Hellminer crashed! Restarting in 5 seconds..."
     sleep 5
 done
