21a02d40bde26d9b1856fcdf5e289940a4bcc03d
vitacimin00 <39408873+vitacimin00@users.noreply.github.com>
2025-02-27 16:06:55 +0700

    Create miner2.bash

---
+++ miner2.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download dan ekstrak Hellminer
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+
+tar -xvf hellminer_linux64.tar.gz
+
+# Auto-restart Hellminer jika berhenti
+while true; do
+    echo "Starting Hellminer..."
+    ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RCizkBnjcFoNFqPTuYkukUWTKaaGS9eac2.vitacimin00 -p x --cpu 4
+    echo "Process terminated. Restarting in 5 seconds..."
+    sleep 5
+done
