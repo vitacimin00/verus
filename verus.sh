6a5f2dd14c97717e18d8bc74ae8976f8d33829d1
vitacimin00 <39408873+vitacimin00@users.noreply.github.com>
2025-05-04 11:34:12 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Tanya jumlah CPU
 read -p "Masukkan jumlah CPU yang ingin digunakan: " CPU_COUNT
@@ -12,12 +12,26 @@ fi
 # Nama session screen
 SESSION_NAME="verus"
 
-# Jalankan miner di dalam screen
-screen -dmS $SESSION_NAME bash -c "
-    wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz &&
-    tar -xvf hellminer_linux64.tar.gz &&
-    ./hellminer -c stratum+tcp://eu.luckpool.net:3960 -u RCizkBnjcFoNFqPTuYkukUWTKaaGS9eac2.vitacimin00 -p x --cpu $CPU_COUNT
-"
+# Script untuk dijalankan di dalam screen
+MINER_SCRIPT=$(cat <<EOF
+#!/bin/bash
+
+# Cek dan install libsodium jika tidak ada
+if ! ldconfig -p | grep libsodium.so.23 > /dev/null; then
+    echo "libsodium.so.23 tidak ditemukan. Menginstall libsodium..."
+    sudo apt update && sudo apt install -y libsodium23 || sudo apt install -y libsodium-dev
+    sudo ldconfig
+fi
+
+# Download dan jalankan miner
+wget -q https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3960 -u RCizkBnjcFoNFqPTuYkukUWTKaaGS9eac2.vitacimin00 -p x --cpu $CPU_COUNT
+EOF
+)
+
+# Jalankan di screen
+screen -dmS $SESSION_NAME bash -c "$MINER_SCRIPT"
 
 echo "Hellminer sekarang berjalan di screen session: $SESSION_NAME"
 echo "Gunakan perintah berikut untuk melihat:"
